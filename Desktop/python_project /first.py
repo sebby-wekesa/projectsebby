f1c72bfea745d0e9eb8a62252e715e04eb856b06
Sebby wekesa <sebbywakis@email.com>
2023-11-18 10:19:40 +0300

    first commit

---
+++ Desktop/python_project /first.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+from calendar import*
+year = int(input("Enter year: "))
+print(calendar(year, 3, 1, 8, 3))
+
+#3 = 3 character for the days (mon , tue,....etc)
+#1 = 1 line(row) for each week
+#8 = 8 rows for each month
+#3 = 3 columns for all the month of the year
+
+
+#wekeCoding
